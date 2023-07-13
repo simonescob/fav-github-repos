b96e8963fd444205a7c2177877b3d9f7eb2835e7
Simon <snsimonescobar@gmail.com>
2023-07-13 02:32:42 -0500

    code access changed

--- src/services/githubService.tsx
+++ src/services/githubService.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const GITHUB_API_URL = 'https://api.github.com/graphql';
-const PERSONAL_ACCESS_TOKEN = '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';
+// const PERSONAL_ACCESS_TOKEN = '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';
+// const PERSONAL_ACCESS_TOKEN = '9ff94b7ed643aab675d855635de29af1a4b4de8c';
+const PERSONAL_ACCESS_TOKEN = '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';
 
 export const fetchGithubData = async (): Promise<any> => {
 
