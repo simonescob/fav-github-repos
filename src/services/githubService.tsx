3231e0a23479d1f33d03a10c5503f7833ac9682f
Simon <snsimonescobar@gmail.com>
2023-07-13 21:00:18 -0500

    token changed

--- src/services/githubService.tsx
+++ src/services/githubService.tsx
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const GITHUB_API_URL = 'https://api.github.com/graphql';
-// const PERSONAL_ACCESS_TOKEN = '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';
-// const PERSONAL_ACCESS_TOKEN = '9ff94b7ed643aab675d855635de29af1a4b4de8c';
-// const PERSONAL_ACCESS_TOKEN = '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';
-const PERSONAL_ACCESS_TOKEN = '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';
+const PERSONAL_ACCESS_TOKEN = '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';
 
 export const fetchGithubData = async (): Promise<any> => {
 
